5b1936941c761318218358a34b3c2fd725fb509a
primo-aishwarya <primo.aishwaryabairagi@gmail.com>
2026-03-12 11:27:16 +0530

    db fix

--- config/db.js
+++ config/db.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: "mysql.dev-zilla.com",
+/*const db = mysql.createConnection({
+  host: "dev-zilla.com",
   user: "u498357097_invoicemaker",
   password: "ayVILQzR05O^",
   database: "u498357097_invoicemaker",
+  port: 3306
+});*/
+const db = mysql.createConnection({
+  host: "72.167.52.232",
+  user: "invoiceuser",
+  password: "invoice@123",
+  database: "odgtv1_invoice",
   port: 3306
 });
 
