e2cc3438d4833048e76e57fb39ef4a2223e877a0
primo-aishwarya <primo.aishwaryabairagi@gmail.com>
2026-03-17 16:00:44 +0530

    updated database connection

--- config/db.js
+++ config/db.js
@@ -1,20 +1,20 @@
 const mysql = require("mysql2");
 
-/*const db = mysql.createConnection({
+const db = mysql.createConnection({
   host: "77.37.48.35",
   user: "u498357097_invoicemaker",
   password: "ayVILQzR05O^",
   database: "u498357097_invoicemaker",
-  port: 3306
-});*/
-const db = mysql.createConnection({
+  port: 43001
+});
+/*const db = mysql.createConnection({
   host: "junction.proxy.rlwy.net",
   user: "root",
   password: "JjWgQBZSRgswbflieUJQLpsYtiCCtsyk",
   database: "railway",
   port: 43001
 });
-
+*/
 
 db.connect((err) => {
   if (err) {
